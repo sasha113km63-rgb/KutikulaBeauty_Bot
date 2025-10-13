d22dce366e4546bf41ac483b0aba2b11e9f9f54b
sasha113km63-rgb <sasha113km63@gmail.com>
2025-10-13 23:19:59 +0400

    Update main.py

--- main.py
+++ main.py
@@ -89,7 +89,7 @@ def home():
 
 
 # ------------------- Запуск -------------------
-if name == "__main__":
+if __name__ == "__main__":
     import uvicorn
     port = int(os.environ.get("PORT", 10000))
     uvicorn.run(app, host="0.0.0.0", port=port)
