792e37e0beccce4a85cbd4a6181d30a2db3a186c
sasha113km63-rgb <sasha113km63@gmail.com>
2025-12-21 22:13:36 +0400

    Delete start_webhook.sh

--- start_webhook.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-TOKEN="7674592997:AAEWsGG55yVxcnH5RoaHNHRdDLRkDpiNLmkE"
-BASE_URL="https://kutikulabeauty-bot.onrender.com"
-
-curl -X POST "https://api.telegram.org/bot${TOKEN}/setWebhook" \
-     -d "url=${BASE_URL}/telegram-webhook"
-
-echo "Webhook set to ${BASE_URL}/telegram-webhook"
